cf13569293b0974a94007a89bb9257b89a8b8054
Abbas Attarwala <attarwal@bu.edu>
2019-05-26 12:46:33 -0400

    changed the package name to make it consistent

---
+++ Assignments/assignment1/src/main/java/csc/summer2019/cscc01/App.java
@@ -0,0 +1,13 @@
+package csc.summer2019.cscc01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Assignments/assignment1/src/test/java/csc/summer2019/cscc01/AppTest.java
@@ -0,0 +1,20 @@
+package csc.summer2019.cscc01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
