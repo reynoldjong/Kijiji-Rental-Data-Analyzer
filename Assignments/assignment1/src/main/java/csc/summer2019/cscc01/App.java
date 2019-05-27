056ec0c47ab256189f98737ed5fda42c468a1b8a
chrling <chr.ling@alumni.utoronto.ca>
2019-05-26 22:17:45 -0400

    finished code for the html parser

--- Assignments/assignment1/src/main/java/csc/summer2019/cscc01/App.java
+++ Assignments/assignment1/src/main/java/csc/summer2019/cscc01/App.java
@@ -1,13 +1,40 @@
 package csc.summer2019.cscc01;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+  public static void main(String[] args) throws IOException {
+    // for each input, try connecting to the website
+    // ==== testing ====
+    //String[] temp = new String[1];
+    //temp[0] = "http://www.simplehtmlguide.com/";
+ // ==== testing ====
+    for (String url : args) {
+      try {
+        Document doc = Jsoup.connect(url).get();
+        // print title, grab all the links on this webpage and print them out
+        String title = doc.title();
+        System.out.println(title + "\n");
+        Elements links = doc.getElementsByTag("a");
+        for (Element link : links) {
+          String linkHref = link.attr("href");
+          System.out.println("link : " + linkHref);
+          String linkText = link.text();
+          System.out.println("text : "+ linkText);
+          // add empty line
+          System.out.println("");
+        }
+      } catch (IOException E) {
+        System.out.println("invalid URL");
+      }
     }
+  }
 }
