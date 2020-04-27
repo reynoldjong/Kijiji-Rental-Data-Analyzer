974d66f7509dfbd060369f1482ef87ce7bb2bf1d
jongtsun <reynold.jong@mail.utoronto.ca>
2020-04-27 18:34:41 -0400

    Modified and repackaged the classes related to Google GeoCoding Engine

--- src/main/java/assignment3/GoogleGeoCodingEngine/CoordinatesEngine.java
+++ src/main/java/assignment3/GoogleGeoCodingEngine/CoordinatesEngine.java
@@ -1,37 +1,46 @@
-package assignment3;
+package assignment3.GoogleGeoCodingEngine;
 
+import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A class that will get the coordinates of a specific address using GeoCoding class
  */
 public class CoordinatesEngine {
 
+    private GeoApiContext context;
+
+    public CoordinatesEngine(GeoCoding geoCoding) {
+        this.context = geoCoding.getContext();
+    }
+
+
     /**
      * Get the coordinates of the inputted address
      * @param address
      */
-    public String getCoordinates(String address) {
-        String coordinateJson = "";
+    public Map<String, String> getCoordinates(String address) {
+        Map<String, String> coordinates = new HashMap<>();
         try {
             // Build the GeoCoding for getting coordinates
-            GeoCoding geoCoding = GeoCoding.buildMapControl();
-            GeocodingResult[] result = GeocodingApi.geocode(geoCoding.getContext(), address).language("en").await();
+            GeocodingResult[] result = GeocodingApi.geocode(context, address).language("en").await();
             // Latitude of the address
             double lat = result[0].geometry.location.lat;
             // Longitude of the address
+            coordinates.put("lat", Double.toString(lat));
             double lng = result[0].geometry.location.lng;
+            coordinates.put("lng", Double.toString(lng));
             // Json format
-            coordinateJson = "\"lat\":\"" + lat + "\",\"lng\":\"" + lng + "\"";
-
         } catch (ApiException | InterruptedException | IOException e) {
             e.printStackTrace();
         }
 
-        return coordinateJson;
+        return coordinates;
     }
 
 }
--- src/main/java/assignment3/GoogleGeoCodingEngine/GeoCoding.java
+++ src/main/java/assignment3/GoogleGeoCodingEngine/GeoCoding.java
@@ -1,4 +1,4 @@
-package assignment3;
+package assignment3.GoogleGeoCodingEngine;
 
 import com.google.maps.GeoApiContext;
 
